3f126a99d9eac9e63de5fc0667fa1ad1f3b70df9
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-09-22 19:29:34 +0530

    Create Theme Context

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,12 +1,39 @@
-import { createContext } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { createContext, useEffect, useState } from "react";
+import { useColorScheme } from "react-native";
 
-export type ThemeOption = 'light' | 'dark' | 'system';
-const THEME_KEY = '@app_color_scheme';
+export type ThemeOption = "light" | "dark" | "system";
+const THEME_KEY = "@app_color_scheme";
 
 type ThemeContextType = {
-    preference: ThemeOption;
-    applied: 'light' | 'dark';
-    setPreference: (themeOption: ThemeOption) => Promise<void>;
+  preference: ThemeOption;
+  applied: "light" | "dark";
+  setPreference: (themeOption: ThemeOption) => Promise<void>;
 };
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ childern }: { childern: React.ReactNode }) {
+  const { getColorScheme, setColorScheme } = useColorScheme();
+  const [getpreference, setpreference] = useState<ThemeOption>("system");
+  const [isReady, setReady] = useState(false);
+
+  useEffect(() => {
+    () => {
+      try {
+        const SavedTheme = await AsyncStorage.getItem(THEME_KEY);
+        if (SavedTheme === "light" || SavedTheme === "dark") {
+          setpreference(SavedTheme);
+          setColorScheme(SavedTheme);
+        } else {
+          setpreference("system");
+          setColorScheme("system");
+        }
+      } catch (error) {
+        console.log("Error loading theme preference:", error);
+      } finally {
+        setReady(true);
+      }
+    };
+  }, [setColorScheme]);
+}
