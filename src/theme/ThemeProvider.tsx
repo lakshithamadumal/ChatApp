c10be96765525302d5416dc1165f037ca51c97ec
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-09-22 18:55:44 +0530

    ThemeContext

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,1 +1,12 @@
+import { createContext } from "react";
+
 export type ThemeOption = 'light' | 'dark' | 'system';
+const THEME_KEY = '@app_color_scheme';
+
+type ThemeContextType = {
+    preference: ThemeOption;
+    applied: 'light' | 'dark';
+    setPreference: (themeOption: ThemeOption) => Promise<void>;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
