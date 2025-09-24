a928c2ac06369c015856121cb065e8e14eeb8a1f
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-09-24 18:04:38 +0530

    Update ContactScreen.tsx

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -6,6 +6,7 @@ import {
   Image,
   Text,
   Pressable,
+  TextInput,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../../global.css";
@@ -25,43 +26,58 @@ export default function ContactScreen() {
         keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 100}
       >
         <View className="p-5 items-center">
-          <Image
-            source={require("../../assets/logoDark.png")}
-            className="h-40 w-36"
-          />
-        </View>
-        <View>
-          <Text className="text-slate-600 font-bold">
-            We Use Your Contacts to find the Friends.
-          </Text>
-        </View>
-        <View className="mt-5 w-full">
-          <Pressable
-            className="w-full justify-center items-center h-16 border-b-4 border-b-green-600"
-            onPress={() => setShow(true)}
-          >
-            <View className="flex-row items-center">
-              <Text className="font-bold text-lg">Select Country</Text>
-              <AntDesign
-                name="caret-down"
-                size={20}
-                color="black"
-                style={{ marginLeft: 8 }}
+          <View>
+            <Image
+              source={require("../../assets/logoDark.png")}
+              className="h-40 w-36"
+            />
+          </View>
+
+          <View>
+            <Text className="text-slate-600 font-bold">
+              We Use Your Contacts to find the Friends.
+            </Text>
+          </View>
+          <View className="mt-5 w-full">
+            <Pressable
+              className="w-full justify-center items-center h-16 border-b-4 border-b-green-600"
+              onPress={() => setShow(true)}
+            >
+              <View className="flex-row items-center">
+                <Text className="font-bold text-lg">Select Country</Text>
+                <AntDesign
+                  name="caret-down"
+                  size={20}
+                  color="black"
+                  style={{ marginLeft: 8 }}
+                />
+              </View>
+            </Pressable>
+            <CountryPicker
+              show={show}
+              lang="en"
+              pickerButtonOnPress={(item) => {
+                setCountryCode(item);
+                setShow(false);
+              }}
+              style={{
+                modal: { height: 400 },
+              }}
+            />
+
+            <View className="mt-2 flex flex-row justify-center">
+              <TextInput
+                inputMode="tel"
+                className="h-16 font-bold text-lg border-y-4 border-y-green-600 w-[18%]"
+                placeholder="+94"
+              />
+              <TextInput
+                inputMode="tel"
+                className="h-16 font-bold text-lg border-y-4 border-y-green-600 w-[80%] ml-2"
+                placeholder="7********"
               />
             </View>
-          </Pressable>
-          <CountryPicker
-            show={show}
-            lang="en"
-            pickerButtonOnPress={(item) => {
-              setCountryCode(item);
-              setShow(false);
-            }}
-
-            style={{
-              modal: { height: 400 },
-            }}
-          />
+          </View>
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
