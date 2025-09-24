0759b0c9e783b992bebe62c33b12fedf9a054c69
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-09-24 16:25:52 +0530

    add Country Picker

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -9,8 +9,14 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../../global.css";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useState } from "react";
+import { CountryItem, CountryPicker } from "react-native-country-codes-picker";
 
 export default function ContactScreen() {
+  const [show, setShow] = useState(false);
+  const [countryCode, setCountryCode] = useState<CountryItem | null>(null);
+
   return (
     <SafeAreaView className="flex-1 bg-red-100 items-center">
       <StatusBar hidden={true} />
@@ -30,9 +36,24 @@ export default function ContactScreen() {
           </Text>
         </View>
         <View className="mt-5 w-full">
-          <Pressable className="bg-red-100 w-full justify-center items-center h-16">
-            <Text className="font-bold text-lg">Select Country</Text>
+          <Pressable
+            className="w-full justify-center items-center h-16 border-b-4 border-b-green-600"
+            onPress={() => setShow(true)}
+          >
+            <View className="flex-row items-center">
+              <Text className="font-bold text-lg">Select Country</Text>
+              <AntDesign
+                name="caret-down"
+                size={20}
+                color="black"
+                style={{ marginLeft: 8 }}
+              />
+            </View>
           </Pressable>
+          <CountryPicker show={show} lang="en" pickerButtonOnPress={() => {}} 
+          style={{
+            modal: { height: 400 },
+          }}/>
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
