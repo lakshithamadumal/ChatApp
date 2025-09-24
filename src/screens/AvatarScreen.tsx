d4c70943e2d7772f7b570af83bdf4e7895d3cbfd
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-09-24 19:09:25 +0530

    AvatarScreen

---
+++ src/screens/AvatarScreen.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function AvatarScreen() {
+  return (
+    <SafeAreaView className="flex-1 bg-red-100 items-center">
+        
+    </SafeAreaView>
+  );
+}
