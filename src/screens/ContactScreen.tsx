a71130dbadca4e5de3fd9a0ef3bbe15cb5718b15
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-09-24 14:44:23 +0530

    Create ContactScreen.tsx

---
+++ src/screens/ContactScreen.tsx
@@ -0,0 +1,12 @@
+import { KeyboardAvoidingView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function ContactScreen() {
+  return (
+  <SafeAreaView>
+    <KeyboardAvoidingView>
+        
+    </KeyboardAvoidingView>
+  </SafeAreaView>
+  );
+}
